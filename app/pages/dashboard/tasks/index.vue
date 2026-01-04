962fddcb8c2d8314e922727da5c73b7bdeda8fd8
Adib Muhammad Maros <adibmaros12@gmail.com>
2026-01-04 18:20:47 +0700

    debug

--- app/pages/dashboard/tasks/index.vue
+++ app/pages/dashboard/tasks/index.vue
@@ -6,20 +6,11 @@ definePageMeta({
 
 const { user } = useUserSession();
 
-// DEBUG: Log user data
-watchEffect(() => {
-  console.log("👤 User session:", user.value);
-  console.log("👤 User ID:", user.value?.id);
-});
-
 const userId = computed(() => user.value?.id);
 </script>
 
 <template>
   <div class="w-full">
-    <!-- DEBUG: Tampilkan userId -->
-    <div class="p-2 bg-yellow-100 text-yellow-800 text-xs mb-2">DEBUG - userId: {{ userId }} | user: {{ user }}</div>
-
     <DraggableList v-if="userId" :user-id="Number(userId)" />
 
     <div v-else class="flex flex-col items-center justify-center py-32">
